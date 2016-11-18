25f197acffe0b5a22a1cb9c0f668912e1bcd27ec
lbuckley <l.b.buckley@gmail.com>
2016-11-18 15:35:51 -0800

    minor updates to database

--- ICBSeasonality_processdata.R
+++ ICBSeasonality_processdata.R
@@ -8,7 +8,7 @@ fdir= "C:\\Users\\Buckley\\Google Drive\\Seasonality\\"
 
 setwd(paste(fdir,"Database\\",sep="") )
 
-dat= read.csv("ThermalDatabase_all_9Nov2016.csv", na.strings="")
+dat= read.csv("ThermalDatabase_all_10Nov2016.csv", na.strings="")
 
 ind_new= paste(dat$"Species",dat$"Author",dat$"Year",dat$"Location_new",sep="_")
 ind_old= paste(dat$"Species",dat$"Author",dat$"Year",dat$"Location",sep="_")
@@ -29,26 +29,31 @@ dat$quality= as.numeric(as.character(dat$quality))
 #--------------------------
 
 #Add foreign language locations
-dat.lang= read.csv("ThermalDatabase_Fall2016_Languages_10Nov2016.csv", na.strings="")
+dat.lang= read.csv("ThermalDatabase_Fall2016_Languages_18Nov2016.csv", na.strings="")
+dat.lang$Location_new= as.character(dat.lang$Location_new)
+dat.lang$comments= as.character(dat.lang$comments)
 
 #change columns to numeric #CHECK CONVERSION
 dat.lang$lon_new= as.numeric(as.character(dat.lang$lon_new))
 dat.lang$lat_new= as.numeric(as.character(dat.lang$lat_new))
 dat.lang$quality= as.numeric(as.character(dat.lang$quality))
  
-match1= match(dat.lang$Order, dat$Order.1)
-dat[match1, c("Location_new","lon_new","lat_new","quality","comments","omit", "colony")]= dat.lang[, c("Location_new","lon_new","lat_new","quality","comments","omit", "colony")]
+dat$Location_new= as.character(dat$Location_new)
+dat$comments= as.character(dat$comments)
+
+match1= match(dat.lang$Order, dat$DatabaseOrder)
+dat[match1, c("Location_new","lon_new","lat_new","quality","comments","omit", "colony","person")]= dat.lang[, c("Location_new","lon_new","lat_new","quality","comments","omit", "colony","person")]
 
 
 #--------------------------
 #Fill in missing new locations with old locations and code quality as 4.
 
-dat$Location_new= as.character(dat$Location_new)
-dat$Location= as.character(dat$Location)
+#dat$Location_new= as.character(dat$Location_new)
+#dat$Location= as.character(dat$Location)
 
-inds= which(is.na(dat$Location_new ))
-dat[inds,"Location_new"]= dat[inds,"Location"]
-dat[inds,"quality"]= 4
+#inds= which(is.na(dat$Location_new ))
+#dat[inds,"Location_new"]= dat[inds,"Location"]
+#dat[inds,"quality"]= 4
 
 #Write out
 write.csv(dat, "ThermalDatabase_updatelocs.csv")
